bb97d0629edb9aff660e624088d7d39382209f79
icejuic3 <jacob.curiel-garfias.1@ens.etsmtl.ca>
2024-02-16 14:45:53 -0500

    entity

--- GPA675_LAB2/Entity.cpp
+++ GPA675_LAB2/Entity.cpp
@@ -1,1 +1,22 @@
 #include "Entity.h"
+
+Entity::Entity(Board& board)
+	:mBoard{ board }
+	, mAge{}
+	, mAlive{ true }
+{
+}
+
+Entity::~Entity()
+{
+}
+
+double Entity::age()
+{
+	return mAge;
+}
+
+void Entity::setDead()
+{
+	mAlive = false;
+}
