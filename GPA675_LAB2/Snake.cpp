dd2cdeeea268fafb2e75c732b8b461e64ce26d1c
MChampagne27 <champagne_racing11@hotmail.com>
2024-02-21 18:48:43 -0500

    Création de classe
    Snake, pellet, obstacle

--- GPA675_LAB2/Snake.cpp
+++ GPA675_LAB2/Snake.cpp
@@ -1,1 +1,183 @@
 #include "Snake.h"
+
+Snake::Snake()
+{
+}
+
+Snake::Snake(Snake::DynamicEntity other)
+{
+}
+
+Snake& Snake::operator=(Snake::DynamicEntity other)
+{
+    // TODO: insérer une instruction return ici
+}
+
+Snake::~Snake()
+{
+}
+
+bool Snake::isValid()
+{
+    return false;
+}
+
+bool Snake::isAlive()
+{
+    return false;
+}
+
+void Snake::ticPrepare(real elapsedTime)
+{
+}
+
+void Snake::ticExecute()
+{
+}
+
+void Snake::draw(QPainter& painter)
+{
+}
+
+bool Snake::isColliding(QPoint const& position)
+{
+    return false;
+}
+
+QString Snake::name()
+{
+    return QString();
+}
+
+int Snake::score()
+{
+    return 0;
+}
+
+size_t Snake::bodyLength()
+{
+    return size_t();
+}
+
+QPoint Snake::headPosition()
+{
+    return QPoint();
+}
+
+Direction Snake::headDirection()
+{
+    return Direction();
+}
+
+SpeedType Snake::speed()
+{
+    return SpeedType();
+}
+
+QColor Snake::headColor()
+{
+    return QColor();
+}
+
+QColor Snake::bodyColor()
+{
+    return QColor();
+}
+
+bool Snake::isReverseProhibited()
+{
+    return false;
+}
+
+Controller& Snake::controller()
+{
+    // TODO: insérer une instruction return ici
+}
+
+void Snake::setName(QString name)
+{
+}
+
+void Snake::reset(QPoint headPosition, direction headDirection, size_t bodyLength, SpeedType initialSpeed)
+{
+}
+
+void Snake::setSpeed(SpeedType speed)
+{
+}
+
+void Snake::increaseSpeed(SpeedType amout)
+{
+}
+
+void Snake::decreaseSpeed(SpeedType amout)
+{
+}
+
+void Snake::decelerate(SpeedType percentLess)
+{
+}
+
+void Snake::accelerate(SpeedType percentMore)
+{
+}
+
+void Snake::setColors(QColor head, QColor body)
+{
+}
+
+void Snake::prohibitedReverse(bool Prohibited)
+{
+}
+
+void Snake::adjustScore(int score)
+{
+}
+
+void Snake::turnRight()
+{
+}
+
+void Snake::turnLeft()
+{
+}
+
+void Snake::goUp()
+{
+}
+
+void Snake::goRight()
+{
+}
+
+void Snake::goDown()
+{
+}
+
+void Snake::goLeft()
+{
+}
+
+void Snake::goToward(Direction direction)
+{
+}
+
+void Snake::grow(size_t size)
+{
+}
+
+void Snake::shrink(size_t size)
+{
+}
+
+Controller::Controller(Snake& snake)
+{
+}
+
+Controller::~Controller()
+{
+}
+
+void Controller::control()
+{
+}
--- GPA675_LAB2/Snake.h
+++ GPA675_LAB2/Snake.h
@@ -1,5 +1,113 @@
 #pragma once
-class Snake
+
+#include"DynamicEntity.h"
+#include <QString>
+
+enum class Direction : int {
+	toUp = 0,
+	toRight = 1,
+	toDown = 2,
+	toLeft = 3
+};
+
+using SpeedType = double;
+
+class Controller
 {
+public:
+	
+	Controller(Snake& snake);
+	virtual ~Controller();
+	virtual void control();
+
+private:
+
+	Snake& mControlledSnake;
+
+	class SnakeKeyboardController
+	{
+
+	};
+
+	class SnakeKeyboardAbsoluteController
+	{
+		snakeKeyboardAbsoluteController();
+
+	};
+
+};
+
+
+class Snake:DynamicEntity
+{
+
+public:
+
+	Snake();
+	Snake(Snake::DynamicEntity other);
+	Snake& operator=(Snake::DynamicEntity other);
+	~Snake();
+
+	bool isValid() override;									//Query
+	bool isAlive() override;									//Query
+	void ticPrepare(real elapsedTime) override;					
+	void ticExecute() override;					
+	void draw(QPainter& painter) override;					
+	bool isColliding(QPoint const& position) override;			//Query
+
+	QString name();												//query
+	int score();												//query
+	size_t bodyLength();										//query
+	QPoint headPosition();										//query
+	Direction headDirection();									//query
+	SpeedType speed();											//query
+	QColor headColor();											//query
+	QColor bodyColor();											//query
+	bool isReverseProhibited();									//query
+	Controller& controller();
+	void setName(QString name);
+	void reset(QPoint headPosition, Direction headDirection, size_t bodyLength, SpeedType initialSpeed);
+	void setSpeed(SpeedType speed);
+	void setColors(QColor head, QColor body);
+	void prohibitedReverse(bool Prohibited);
+	void adjustScore(int score);
+	void turnRight();
+	void turnLeft();
+	void goUp();
+	void goRight();
+	void goDown();
+	void goLeft();
+	void goToward(Direction direction);
+	void grow(size_t size = 1);
+	void shrink(size_t size = 1);
+	void setSpeed(SpeedType speed);
+	void increaseSpeed(SpeedType amout);
+	void decreaseSpeed(SpeedType amout);
+	void decelerate(SpeedType percentLess);
+	void accelerate(SpeedType percentMore);
+
+
+
+private:
+
+	QString mName;
+	int mScore = 0;
+	Body mBody;
+	Direction mHeadDirection = Direction::toUp;
+	SpeedType mSpeed;
+	size_t mSizeToGrow = 0;
+	QColor mHeadColor = QColor(0, 255, 0);
+	QColor mBodyColor = QColor(0, 128, 0);
+	Controller mController = SnakeKeyboardAbsoluteController();
+	bool mReverseProhibited;
+
+	const std::array<Direction, 4> LUTTurnLeftDirection;
+	const std::array<Direction, 4> LUTTurnRightDirection;
+	const std::array<Direction, 4> LUTOppositeDirection;
+	const std::array<QPoint, 4> LUTDirectionDisplacement;
+	const std::array<void(Snake::*)(),4> LUTDirectionAction;
+
+
+
 };
 
