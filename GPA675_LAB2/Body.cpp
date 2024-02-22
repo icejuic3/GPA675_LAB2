8c201209c203f7f45f58bc8e559bef5b707bf5eb
William David-Way <william.david-way.1@ens.etsmtl.ca>
2024-02-21 20:50:14 -0500

    Body reset

---
+++ GPA675_LAB2/Body.cpp
@@ -0,0 +1,135 @@
+#include "Body.h"
+
+Body::Body()
+	: mHead{}
+	, mTail{}
+	, mSize{}
+{
+}
+
+Body::~Body()
+{
+	delete mHead;
+}
+
+bool Body::isEmpty() const
+{
+	return mSize == 0ull;
+}
+
+size_t Body::size() const
+{
+	return mSize;
+}
+
+QPoint* Body::first() const
+{
+	if (mSize == 0) {
+		return nullptr;
+	}
+	else {
+		return &mHead->position;
+	}
+}
+
+QPoint* Body::last() const
+{
+	if (mSize == 0) {
+		return nullptr;
+	}
+	else {
+		return &mTail->position;
+	}
+}
+
+void Body::addFirst(QPoint position)
+{
+	mHead = new BodyPart(position, mHead, nullptr);
+	++mSize;
+}
+
+void Body::addLast(QPoint position)
+{
+	if (mHead == nullptr) { // la liste est vide
+		addFirst(position);
+	}
+	else {
+		mTail = new BodyPart(position, nullptr, mTail);
+		++mSize;
+	}
+}
+
+void Body::add(size_t index, QPoint position)
+{
+}
+
+void Body::removeFirst()
+{
+	if (!mHead) {
+		return;
+	}
+	BodyPart* temp{ mHead };
+	mHead = mHead->next;
+	temp->next = nullptr;
+	delete temp;
+	--mSize;
+}
+
+void Body::removeLast()
+{
+	if (!mTail) {
+		return;
+	}
+	BodyPart* temp{ mTail };
+	mTail = mTail->previous;
+	temp->previous = nullptr;
+	delete temp;
+	--mSize;
+	
+}
+
+void Body::remove(size_t index)
+{
+
+}
+
+void Body::clear()
+{
+	delete mHead;
+	mHead = nullptr;
+	mSize = 0ull;
+}
+
+void Body::swap(size_t index0, QPoint position)
+{
+}
+
+void Body::swap(Body& otherBody)
+{
+	std::swap(mHead, otherBody.mHead);
+	std::swap(mSize, otherBody.mSize);
+}
+
+void Body::rotate(int indexFrom)
+{
+	if (indexFrom > mSize / 2) {
+		//parcour par la fin
+		BodyPart* tail{ mTail };
+		for (int i{ 0 }; i < (mSize - indexFrom); ++i) {
+			
+		}
+	}
+	else {
+		//parcour par le debut
+	}
+}
+
+bool Body::isColliding(QPoint const& position) const
+{
+	return false;
+}
+
+void Body::draw(QPainter& painter)
+{
+	painter.setPen(Qt::NoPen);
+}
